4abc398f711439bde950e6ed8102097c3d89f0c0
Nilton da Silva Junior <tonsti@gmail.com>
2020-10-12 12:27:18 +0200

    Add navbar 2

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './core/assets/styles/custom.scss';
 import './app.scss';
+import Navbar from './core/components/Navbar';
 
 const App = () => {
     return (    
-        <App />
+        <Navbar />
     );
 }
 
