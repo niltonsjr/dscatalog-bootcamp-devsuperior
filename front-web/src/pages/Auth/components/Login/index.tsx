9a1a5f2c5673e42ecd222c385e89f7c82b31a038
Nilton da Silva Junior <tonsti@gmail.com>
2020-11-13 21:19:05 +0100

    Finish login/logout layout

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -79,7 +79,7 @@ const Login = () => {
           )}
         </div>
 
-        <Link to="/admin/auth/recover" className="login-link-recover">
+        <Link to="/auth/recover" className="login-link-recover">
           Esqueci a senha?
         </Link>
         <div className="login-submit">
@@ -87,7 +87,7 @@ const Login = () => {
         </div>
         <div className="text-center">
           <span className="not-registered">Não tem Cadastro?</span>
-          <Link to="/admin/auth/register" className="login-link-register">
+          <Link to="/auth/register" className="login-link-register">
             CADASTRAR
           </Link>
         </div>
