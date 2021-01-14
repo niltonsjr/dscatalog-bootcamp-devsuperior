b7204ee30510045cbaff64a436b60ad78ddca978
Nilton da Silva Junior <tonsti@gmail.com>
2021-01-14 20:30:54 +0100

    Add Product price Test

---
+++ front-web/src/core/components/ProductPrice/__tests__/ProductPrice.spec.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import ProductPrice from "..";
+
+test("Should render ProductPrice", () => {
+  const price = 1200;
+  render(
+    <ProductPrice price={price} />
+  );
+  screen.debug();
+  const currencyElement = screen.getByText('R$');
+  const priceElement = screen.getByText('1,200.00');
+  expect(currencyElement).toBeInTheDocument();
+  expect(priceElement).toBeInTheDocument();
+});
+
+test("Should render ProductPrice with price equals to zero", () => {
+  const price = 0;
+  render(
+    <ProductPrice price={price} />
+  );
+  screen.debug();
+  const currencyElement = screen.getByText('R$');
+  const priceElement = screen.getByText('0.00');
+  expect(currencyElement).toBeInTheDocument();
+  expect(priceElement).toBeInTheDocument();
+});
+
+test("Should render ProductPrice with price without thousand separator", () => {
+  const price = 100;
+  render(
+    <ProductPrice price={price} />
+  );
+  screen.debug();
+  const currencyElement = screen.getByText('R$');
+  const priceElement = screen.getByText('100.00');
+  expect(currencyElement).toBeInTheDocument();
+  expect(priceElement).toBeInTheDocument();
+});
--- front-web/src/core/utils/__tests__/list.spec.ts
+++ front-web/src/core/utils/__tests__/list.spec.ts
@@ -1,19 +1,13 @@
 import {generateList} from '../list';
 
 test('should generate a list', () => {
-    //Arrange
     const amount = 5;
-    //Act
     const result = generateList(amount);
-    //Assert    
     expect(result).toEqual([0, 1, 2, 3, 4]);
 });
 
 test('should generate an empty list when amount is zero', () => {
-    //Arrange
     const amount = 0;
-    //Act
     const result = generateList(amount);
-    //Assert    
     expect(result).toEqual([]);
 });
