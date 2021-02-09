19b2a3b24347e61f721da9afc07a5e2d6750d2b3
Nilton da Silva Junior <tonsti@gmail.com>
2021-02-09 22:05:37 +0100

    Add Home page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,5 @@
 import "react-native-gesture-handler";
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./src/routes";
 
--- mobile/src/pages/Home.tsx
+++ mobile/src/pages/Home.tsx
@@ -1,21 +1,35 @@
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { theme, text } from "../styles";
+import arrow from "../assets/arrow.png";
+import draw from "../assets/draw.png";
 
-const Home: React.FC = ({ navigation }) => {
+const Home: React.FC = ({}) => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>Ben vindo al APP</Text>
-      <TouchableOpacity
-        style={{
-          width: 150,
-          backgroundColor: "#069",
-          padding: 10,
-          borderRadius: 4,
-        }}
-        onPress={() => navigation.navigate("Catalog")}
-      >
-        <Text>Clique aqui</Text>
-      </TouchableOpacity>
+    <View style={theme.container}>
+      <View style={theme.card}>
+        <Image source={draw} style={theme.draw} />
+        <View style={theme.textContainer}>
+          <Text style={text.bold}>Conheça o melhor catálogo de produtos</Text>
+          <Text style={text.regular}>
+            Ajudaremos você a encontrar os melhores produtos disponíveis no
+            mercado.
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={theme.primaryButton}
+          activeOpacity={0.8}
+          onPress={() => navigation.navigate("Catalog")}
+        >
+          <Text style={text.primaryText}>INICIE AGORA A SUA BUSCA</Text>
+          <View style={theme.arrowContainer}>
+            <Image source={arrow} />
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
