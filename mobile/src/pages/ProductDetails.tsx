e522258070f872e030c93614d8ef370ad25d7ba6
Nilton da Silva Junior <tonsti@gmail.com>
2021-02-17 21:36:58 +0100

    Add style to Product Details page

--- mobile/src/pages/ProductDetails.tsx
+++ mobile/src/pages/ProductDetails.tsx
@@ -3,12 +3,15 @@ import { Text, View, ActivityIndicator, Image } from "react-native";
 import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 import { api } from "../services";
 import arrow from '../assets/leftArrow.png';
+import { text, theme } from "../styles";
+import { useNavigation } from "@react-navigation/native";
 
 const ProductDetils = ({
   route: {
     params: { id },
   },
 }) => {
+  const navigation = useNavigation();
   const [product, setProduct] = useState({
     id: null,
     name: null,
@@ -33,25 +36,25 @@ const ProductDetils = ({
   }, []);
 
   return (
-    <View>
+    <View style={theme.detailContainer}> 
         {
         loading ? <ActivityIndicator size="large" /> 
         : 
-        <View>
-            <TouchableOpacity>
+        <View style={theme.detailCard}> 
+            <TouchableOpacity style={theme.goBackContainer} onPress={()=> navigation.goBack()}>
                 <Image source={arrow} />
-                <Text>Voltar</Text>
+                <Text style={text.goBackText}>Voltar</Text>
             </TouchableOpacity>
-            <View>
-                <Image source={{uri: product.imgUrl}} style={{width:150, height:150}} />
+            <View style={theme.productImageContainer}>
+                <Image source={{uri: product.imgUrl}} style={theme.productImage} />
             </View>
-            <Text>{product.name}</Text>
-            <View>
-                <Text>R$</Text>
-                <Text>{product.price}</Text>
+            <Text style={text.productDetailsName}>{product.name}</Text>
+            <View style={theme.priceContainer}>
+                <Text style={text.currency}>R$</Text>
+                <Text style={text.productPrice}>{product.price}</Text>
             </View>
-            <ScrollView>
-                <Text>{product.description}</Text>
+            <ScrollView style={theme.scrollTextContainer}>
+                <Text style={text.productDescription}>{product.description}</Text>
             </ScrollView>
         </View>
     }</View>
--- mobile/src/styles/index.ts
+++ mobile/src/styles/index.ts
@@ -49,6 +49,24 @@ const text = StyleSheet.create({
         color: colors.primary,
         fontWeight: "bold",
     },
+    goBackText:{
+        fontSize: 18,
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        color: colors.darkGray,
+        marginLeft: 16,
+    },
+    productDetailsName:{
+        fontSize: 30,
+        fontWeight: "bold",
+        marginTop: 10,
+        color: colors.darkGray,
+    },
+    productDescription:{
+        fontSize: 16,
+        fontWeight: "400",
+        color: colors.mediumGray,
+    }
 });
 
 const theme = StyleSheet.create({
@@ -58,8 +76,6 @@ const theme = StyleSheet.create({
         alignItems: "center",
         padding: 20,
     },
-
-
 
     card: {
         width: "100%",
@@ -76,13 +92,16 @@ const theme = StyleSheet.create({
         shadowRadius: 3.84,
         justifyContent: "space-around",
     },
+
     draw: {
         width: 313,
         height: 225,
     },
+
     textContainer: {
         paddingHorizontal: 20,
     },
+
     primaryButton: {
         width: 290,
         height: 50,
@@ -93,6 +112,7 @@ const theme = StyleSheet.create({
         borderRadius: 10,
 
     },
+
     arrowContainer: {
         height: 50,
         width: 50,
@@ -125,17 +145,20 @@ const theme = StyleSheet.create({
         justifyContent: "space-around",
         marginVertical: 10,
     },
+
     productDescription: {
         width: "100%",
         padding: 20,
         borderTopColor: colors.lightGray,
         borderTopWidth: 1,
     },
-    priceContainer:{
+
+    priceContainer: {
         flexDirection: "row",
         marginTop: 10,
     },
-    productImg:{
+
+    productImg: {
         width: 140,
         height: 140,
         margin: 16,
@@ -158,12 +181,66 @@ const theme = StyleSheet.create({
         marginVertical: 12.5,
         paddingVertical: 10,
     },
+
     serchInput: {
         width: "90%",
         height: 40,
         borderBottomWidth: 0.5,
         borderColor: colors.borderGray,
     },
+
+    //Product Details
+
+    detailContainer: {
+        backgroundColor: colors.lightGray,
+        padding: 20,
+    },
+
+    detailCard: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: colors.white,
+        borderRadius: 20,
+        shadowColor: colors.black,
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        justifyContent: "space-around",
+        padding: 20,
+    },
+
+    goBackContainer: {
+        width: 290,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        marginVertical: 10,
+    },
+
+    productImageContainer: {
+        width: "100%",
+        borderWidth: 1,
+        borderColor: colors.lightGray,
+        alignItems: "center",
+        borderRadius: 20,
+    },
+
+    productImage: {
+        width: 220,
+        height: 220,
+    },
+
+    scrollTextContainer:{
+        marginVertical: 20,
+        padding: 20,
+        borderWidth: 0.5,
+        borderRadius: 15,
+        borderColor: colors.lightGray,
+    }
+
 
 });
 
