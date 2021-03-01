e6fc5e2b7e339e0096ed1e00f1437354cd59f55a
Nilton da Silva Junior <tonsti@gmail.com>
2021-03-01 21:30:55 +0100

    Finish Product Filters

--- front-web/src/core/components/ProductFilters/index.tsx
+++ front-web/src/core/components/ProductFilters/index.tsx
@@ -1,20 +1,65 @@
-import React from 'react';
-import './styles.scss'
-import { ReactComponent as SearchIcon } from 'core/assets/images/search-icon.svg'
+import React, { useEffect, useState } from "react";
+import "./styles.scss";
+import { ReactComponent as SearchIcon } from "core/assets/images/search-icon.svg";
+import Select from "react-select";
+import { Category } from "core/types/Product";
+import { makeRequest } from "core/utils/request";
 
-const ProductFilters = () => {
-    return (
-        <div className="card-base product-filters-container">
-            <div className="input-search">
-                <input 
-                    type="text" 
-                    className="form-control"
-                    placeholder="Pesquisar produto"
-                />
-                <SearchIcon/>
-            </div>
-        </div>
-    )
-}
+type Props = {
+  name?: string;
+  category?: Category;
+  handleChangeName: (name: string) => void;
+  handleChangeCategory: (category: Category) => void;
+  clearFilters: () => void;  
+};
+
+const ProductFilters = ({ name, handleChangeName, category, handleChangeCategory, clearFilters }: Props) => {
+  const [isLoadingCategories, setIsloadingCategories] = useState(false);
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    setIsloadingCategories(true);
+    makeRequest({ url: "/categories" })
+      .then((response) => setCategories(response.data.content))
+      .finally(() => setIsloadingCategories(false));
+  }, []);
+
+  return (
+    <div className="card-base product-filters-container">
+      <div className="input-search">
+        <input
+          type="text"
+          value={name}
+          className="form-control"
+          placeholder="Pesquisar produto"
+          onChange={(event) => handleChangeName(event.target.value)}
+        />
+        <SearchIcon />
+      </div>
+
+      <Select
+        name="categories"
+        key={`select-${category?.id}`}
+        value={category}
+        isLoading={isLoadingCategories}
+        options={categories}
+        getOptionLabel={(option: Category) => option.name}
+        getOptionValue={(option: Category) => String(option.id)}
+        className="filter-select-container"
+        classNamePrefix="product-categories-select"
+        placeholder="Categorias"
+        inputId="categories"
+        onChange={(value) => handleChangeCategory(value as Category)}
+        isClearable
+      />
+      <button
+        className="btn btn-outline-secondary border-radius-10"
+        onClick={clearFilters}
+      >
+        LIMPAR FILTRO
+      </button>
+    </div>
+  );
+};
 
 export default ProductFilters;
--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import ProductCard from "./components/ProductCard";
 import "./styles.scss";
 import { makeRequest } from "core/utils/request";
-import { ProductsResponse } from "core/types/Product";
+import { Category, ProductsResponse } from "core/types/Product";
 import ProductCardLoader from "./components/Loaders/ProductCardLoader";
 import Pagination from "core/components/Pagination";
 import ProductFilters from "core/components/ProductFilters";
@@ -12,26 +12,56 @@ const Catalog = () => {
   const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
   const [isLoading, setIsLoading] = useState(false);
   const [activePage, setActivePage] = useState(0);
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState<Category>();
+
+  const getProducts = useCallback(() => {
+      const params = {
+        page: activePage,
+        linesPerPage: 12,
+        name,
+        categoryId: category?.id,
+      };
+
+      setIsLoading(true);
+      makeRequest({ url: "/products", params })
+        .then((response) => setProductsResponse(response.data))
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }, [activePage, name, category]);
 
   useEffect(() => {
-    const params = {
-      page: activePage,
-      linesPerPage: 12,
-    };
+    getProducts();
+  }, [getProducts]);
 
-    setIsLoading(true);
-    makeRequest({ url: "/products", params })
-      .then((response) => setProductsResponse(response.data))
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, [activePage]);
+  const handleChangeName = (name: string) => {
+    setActivePage(0);
+    setName(name);
+  };
+
+  const handleChangeCategory = (category: Category) => {
+    setActivePage(0);
+    setCategory(category);
+  };
+
+  const clearFilters = () => {
+    setActivePage(0);
+    setCategory(undefined);
+    setName("");
+  };
 
   return (
     <div className="catalog-container">
       <div className="d-flex justify-content-between">
         <h1 className="catalog-title">Catalogo de produtos</h1>
-        <ProductFilters />
+        <ProductFilters 
+          name={name} 
+          category={category}
+          handleChangeCategory={handleChangeCategory}
+          handleChangeName={handleChangeName}
+          clearFilters={clearFilters}
+        />
       </div>
 
       <div className="catalog-products">
